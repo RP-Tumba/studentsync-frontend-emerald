3c283da66a8c7600343a98b51cd5af093f634300
Nziza Oscar <santosjuniorsoftwareengineer1@mail.com>
2025-05-13 02:52:12 +0300

    feat: banner structure left side

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,20 @@
 const Home = () => {
-    return <>
-        <h3>Home page</h3>
-    </>
-}
-export default Home
+  return (
+    <div className="container">
+      <div className="intro">
+        <div className="left-side">
+          <h4 className="text-light-green">Students management platform </h4>
+          <h1 className="font-bold text-green-600">STUDENTSYNC MANAGEMENT WEB APP</h1>
+          <h5 className="text-amber">Students for the Academic Year 2025/2026</h5>
+
+          <div>
+            <button className="btn btn-outline-green">Learn More</button>
+            <button className="btn btn-green">All students</button>
+          </div>
+        </div>
+        <div className="right-side"></div>
+      </div>
+    </div>
+  );
+};
+export default Home;
