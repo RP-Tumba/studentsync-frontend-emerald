4a84d3b7e2527e3f8ab584a3637d6e34678bade7
The Gym Rwanda <The Gym Rwanda@TUMBALAB3>
2025-05-12 16:34:22 +0200

    refactor : Addition of Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,31 @@
+import logo from '../assets/logo-smait.png';
+import { Link } from 'react-router-dom';
+import './Navbar.css'
 const Navbar = () => {
-  return <nav>Navbar</nav>;
+  return (
+    <div>
+      {
+        <div className="nav-main">
+          <div className="nav-left">
+            <div className="nav-image">
+
+              <img src={logo} alt="" className='nav-log-image' />
+            </div>
+            <div className="nav-content">
+              <p>STUDENTSYNC</p>
+            </div>
+          </div>
+          <div className="nav-right">
+            <ul>
+              <li><Link to="/" className='Home-style'>Home</Link ></li>
+              <li><Link to="/AddStudent"className='AddStudent-style'>Add Student</Link></li>
+              <li><Link to="/AllStudents"><button className='AllStudents-style'>All Students</button></Link></li>
+            </ul>
+          </div>
+        </div>
+      }
+    </div>
+  )
 };
 
 export default Navbar;
