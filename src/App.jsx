048d5e5ab7b669e52e5320bb42e5aab21d71fac1
dosite-dev <dositecyiza@gmail.com>
2025-05-12 19:56:41 +0200

    feat: Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import StudentList from "./pages/StudentList";
+import Footer from "./components/Footer";
 import "./App.css";
 
 const App = () => {
   return (
-    <Router>
-      <div className="app-container">
-        <Navbar />
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<StudentList />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <footer>
+      <Footer />
+    </footer>
   );
 };
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,70 @@
+import { IoLocationSharp } from "react-icons/io5";
+import { FaClock } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { FaPhone } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+
+import "../Footer.css";
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="Footer-main">
+        <div className="left-footer-content">
+          <h3>STUDENTSYNC MANAGEMENT APP</h3>
+          <p className="paragraph1">Student management platform</p>
+          <br />
+          <h4>Our Address</h4>
+          <p className="paragraph2">
+            {" "}
+            <IoLocationSharp />
+            Gasabo District, Kinyinya Sector Murama Cell, Rusenyi Village
+            <br />
+            KG 29 Ave 16, Kigali, Rwanda
+          </p>{" "}
+          <br />
+          <p>
+            {" "}
+            <FaClock /> Working Hours: Monday - Friday 08:00 - 17:00
+          </p>
+        </div>
+
+        <div className="middle-footer-content">
+          <h4>Contact us</h4>
+          <p className="paragraph3">
+            <MdEmail /> info@thegymcapstone.rw
+          </p>
+          <p className="paragraph4">
+            {" "}
+            <FaPhone className="phone-icon" /> 0788-322-223
+          </p>
+        </div>
+
+        <div className="right-footer-content">
+          <h4>Social media</h4>
+          <ul>
+            <li>
+              <FaInstagram />
+            </li>
+            <li>
+              {" "}
+              <FaFacebook />
+            </li>
+            <li>
+              {" "}
+              <FaYoutube />{" "}
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="footer-copy-content">
+        <p> &copy; Copyright Students. All Rights Reserved </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
