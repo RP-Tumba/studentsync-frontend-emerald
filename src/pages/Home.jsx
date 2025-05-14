b0a13c8609d2e55ac13eb796259cbef132a68de0
Nziza Oscar <santosjuniorsoftwareengineer1@mail.com>
2025-05-14 14:01:44 +0300

    feat: banner structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,42 @@
+import img1 from '../assets/img-3.png';
+import img2 from '../assets/img-4.png';
+import img3 from '../assets/img-2.png';
+import img4 from '../assets/img-1.png';
+
 const Home = () => {
   return (
     <div className="container">
       <div className="intro">
         <div className="left-side">
-          <h4 className="text-light-green">Students management platform </h4>
-          <h1 className="font-bold text-green-600">STUDENTSYNC MANAGEMENT WEB APP</h1>
+          <h4 className="text-green">Students management platform </h4>
+          <h1 className="font-bold text-gray">STUDENTSYNC MANAGEMENT WEB APP</h1>
           <h5 className="text-amber">Students for the Academic Year 2025/2026</h5>
 
-          <div>
-            <button className="btn btn-outline-green">Learn More</button>
-            <button className="btn btn-green">All students</button>
+          <div className="flex ">
+            <button className="btn-outline-green text-green">Learn More</button>
+            <button className="btn btn-green bg-green text-white">All students</button>
           </div>
         </div>
-        <div className="right-side"></div>
+
+        <div className="right-side">
+          <div>
+            <img src={img1} />
+          </div>
+          <div className="h-b">
+            <img src={img2} alt="" srcset="" />
+          </div>
+          <div className="h-b">
+            <img src={img3} alt="" srcset="" />
+          </div>
+          <div className="h-b">
+            <img src={img4} alt="" srcset="" />
+          </div>
+        </div>
       </div>
+
+      <div className="culture"></div>
+
+      <div className="gallery"></div>
     </div>
   );
 };
