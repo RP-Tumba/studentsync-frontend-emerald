2776d477c644a543e1db0b559fe622ed13cb588e
Nziza Oscar <santosjuniorsoftwareengineer1@mail.com>
2025-05-15 19:31:32 +0300

    feat: display all students

--- src/pages/AllStudents.jsx
+++ src/pages/AllStudents.jsx
@@ -1,13 +1,24 @@
 import { FaSearch, FaRegTrashAlt, FaEdit } from 'react-icons/fa';
-
+import useStudentStore from '../store/studentStore';
+import { useEffect } from 'react';
 const AllStudents = () => {
+  const { students, fetchStudents, loading, error } = useStudentStore();
+
+  useEffect(() => {
+    fetchStudents();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>{error}</div>;
+
   return (
     <div className="container">
       <div className="flex justify-between items-center">
         <h1 className="text-green text-md">All students</h1>
         <div className="flex items-center">
           <div className="search  items-center">
-            <label for="search-input">
+            <label htmlFor="search-input">
               <FaSearch size={20} className="icon" />
             </label>
             <div>
@@ -27,129 +38,35 @@ const AllStudents = () => {
               <th>Username</th>
               <th>Student ID</th>
               <th>Enrollment Date</th>
-              <th>Status</th>
+              <th>Date Birth</th>
+              <th>Contact Number</th>
+              <th>Email</th>
               <th>Action</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Clark Gill</td>
-              <td>123</td>
-              <td>{new Date().toDateString()}</td>
-              <td>Enrolled</td>
-              <td>
-                <div className="action">
-                  <button id="delete">
-                    <FaRegTrashAlt />
-                  </button>
-                  <button id="edit">
-                    <FaEdit />
-                  </button>
-                </div>
-              </td>
-            </tr>
-
-            <tr>
-              <td>Clark Gill</td>
-              <td>123</td>
-              <td>{new Date().toDateString()}</td>
-              <td>Enrolled</td>
-              <td>
-                <div className="action">
-                  <button id="delete">
-                    <FaRegTrashAlt />
-                  </button>
-                  <button id="edit">
-                    <FaEdit />
-                  </button>
-                </div>
-              </td>
-            </tr>
-
-            <tr>
-              <td>Clark Gill</td>
-              <td>123</td>
-              <td>{new Date().toDateString()}</td>
-              <td>Enrolled</td>
-              <td>
-                <div className="action">
-                  <button id="delete">
-                    <FaRegTrashAlt />
-                  </button>
-                  <button id="edit">
-                    <FaEdit />
-                  </button>
-                </div>
-              </td>
-            </tr>
-
-            <tr>
-              <td>Clark Gill</td>
-              <td>123</td>
-              <td>{new Date().toDateString()}</td>
-              <td>Enrolled</td>
-              <td>
-                <div className="action">
-                  <button id="delete">
-                    <FaRegTrashAlt />
-                  </button>
-                  <button id="edit">
-                    <FaEdit />
-                  </button>
-                </div>
-              </td>
-            </tr>
-
-            <tr>
-              <td>Clark Gill</td>
-              <td>123</td>
-              <td>{new Date().toDateString()}</td>
-              <td>Enrolled</td>
-              <td>
-                <div className="action">
-                  <button id="delete">
-                    <FaRegTrashAlt />
-                  </button>
-                  <button id="edit">
-                    <FaEdit />
-                  </button>
-                </div>
-              </td>
-            </tr>
-
-            <tr>
-              <td>Clark Gill</td>
-              <td>123</td>
-              <td>{new Date().toDateString()}</td>
-              <td>Enrolled</td>
-              <td>
-                <div className="action">
-                  <button id="delete">
-                    <FaRegTrashAlt />
-                  </button>
-                  <button id="edit">
-                    <FaEdit />
-                  </button>
-                </div>
-              </td>
-            </tr>
-
-            <tr>
-              <td>Clark Gill</td>
-              <td>123</td>
-              <td>{new Date().toDateString()}</td>
-              <td>Enrolled</td>
-              <td>
-                <div className="action">
-                  <button id="delete">
-                    <FaRegTrashAlt />
-                  </button>
-                  <button id="edit">
-                    <FaEdit />
-                  </button>
-                </div>
-              </td>
-            </tr>
+            {students.map(student => (
+              <tr key={student.id}>
+                <td>
+                  {student.firstName} {student.lastName}
+                </td>
+                <td>{student.studentId}</td>
+                <td>{new Date(student.enrollmentDate).toDateString()}</td>
+                <td>{student.dateOfBirth}</td>
+                <td>{student.contactNumber}</td>
+                <td>{student.email}</td>
+                <td>
+                  <div className="action">
+                    <button id="delete">
+                      <FaRegTrashAlt />
+                    </button>
+                    <button id="edit">
+                      <FaEdit />
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
