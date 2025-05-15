53c99ffcc6872d083b89daabd5a4b22eaa14a3f8
Nziza Oscar <santosjuniorsoftwareengineer1@mail.com>
2025-05-15 05:14:12 +0300

    refactor: apply NavLink active class

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,37 @@
 import logo from '../assets/logo-smait.png';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    
-      
-        <div className="navbar">
-
-          <div className="brand">
-            <div className="nav-image">
-              <img src={logo} alt="" />
-            </div>
-            <div className="brand-title">
-              <h1 className='text-gray'>STUDENTSYNC</h1>
-            </div>
-          </div>
-
-
-
-          <div className="nav-right">
-            <ul>
-              <li><Link to="/" className='text-green'>Home</Link ></li>
-              <li><Link to="/AddStudent"className='text-black'>Add Student</Link></li>
-              <li><Link to="/AllStudents" className='text-green'>
-             <button  className='text-green btn-outline-green' > All Students</button></Link></li>
-
-            </ul>
-          </div>
-
-
+    <div className="navbar">
+      <div className="brand">
+        <div className="nav-image">
+          <img src={logo} alt="" />
         </div>
-      
-  
-  )
+        <div className="brand-title">
+          <h1 className="text-gray">STUDENTSYNC</h1>
+        </div>
+      </div>
+
+      <div className="nav-right">
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/AddStudent" className="text-black">
+              Add Student
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/AllStudents" className="text-green">
+              <button className="text-green btn-outline-green"> All Students</button>
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
