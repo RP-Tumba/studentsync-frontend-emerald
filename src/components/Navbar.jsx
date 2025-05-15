89fe2e53ae64339037f0a043efa93ec86281ed9d
The Gym Rwanda <The Gym Rwanda@TUMBALAB3>
2025-05-15 21:49:51 +0200

    refactor : Add humbug for nav-list

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,18 @@
+import { useState } from 'react';
 import logo from '../assets/logo-smait.png';
 import { Link, NavLink } from 'react-router-dom';
+import { dark } from '@mui/material/styles/createPalette';
+// import MenuIcon from '@material-ui/icons/Menu';
+
 
 const Navbar = () => {
+  const [navData, setNavData] = useState(false)
+  const navMenu = ()=>{
+    setNavData(!navData ? true: false)
+  }
   return (
     <div className="navbar">
+      
       <div className="brand">
         <div className="nav-image">
           <img src={logo} alt="" />
@@ -12,20 +21,23 @@ const Navbar = () => {
           <h1 className="text-gray">STUDENTSYNC</h1>
         </div>
       </div>
-
+           <div className='nav-display-manu' onClick={navMenu}>
+            &#9776;
+            {navData}
+            </div>    
       <div className="nav-right">
-        <ul>
+        <ul className={navData ? "active-nav":""}>
           <li>
-            <NavLink to="/">Home</NavLink>
+            <NavLink to="/" className="text-black">Home</NavLink >
           </li>
           <li>
-            <NavLink to="/AddStudent" className="text-black">
+            <NavLink to="/create-student" className="text-black">
               Add Student
             </NavLink>
           </li>
           <li>
-            <NavLink to="/AllStudents" className="text-green">
-              <button className="text-green btn-outline-green"> All Students</button>
+            <NavLink to="/students" className="text-black">
+              All Students
             </NavLink>
           </li>
         </ul>
