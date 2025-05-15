e8141350df1fd0a2d7493b088f72ac20f867e0a6
Nziza Oscar <santosjuniorsoftwareengineer1@mail.com>
2025-05-15 20:34:20 +0300

    refactor: sticky table header

--- src/pages/AllStudents.jsx
+++ src/pages/AllStudents.jsx
@@ -14,61 +14,63 @@ const AllStudents = () => {
 
   return (
     <div className="container">
-      <div className="flex justify-between items-center">
-        <h1 className="text-green text-md">All students</h1>
-        <div className="flex items-center">
-          <div className="search  items-center">
-            <label htmlFor="search-input">
-              <Search className="icon" />
-            </label>
-            <div>
-              <input type="text" placeholder="Search...." name="term" id="search-input" />
+      <div className="student-list">
+        <div className="flex justify-between items-center student-list-header">
+          <h1 className="text-green text-md">All students</h1>
+          <div className="flex items-center">
+            <div className="search  items-center">
+              <label htmlFor="search-input">
+                <Search className="icon" />
+              </label>
+              <div>
+                <input type="text" placeholder="Search...." name="term" id="search-input" />
+              </div>
+            </div>
+            <div className="addStudent">
+              <button>Add Student</button>
             </div>
           </div>
-          <div className="addStudent">
-            <button>Add Student</button>
-          </div>
         </div>
-      </div>
 
-      <div className="responsive-table">
-        <table cellPadding={0} cellSpacing={0}>
-          <thead>
-            <tr>
-              <th>Username</th>
-              <th>Student ID</th>
-              <th>Enrollment Date</th>
-              <th>Date Birth</th>
-              <th>Contact Number</th>
-              <th>Email</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {students.map(student => (
-              <tr key={student.id}>
-                <td>
-                  {student.firstName} {student.lastName}
-                </td>
-                <td>{student.studentId}</td>
-                <td>{new Date(student.enrollmentDate).toDateString()}</td>
-                <td>{new Date(student.dateOfBirth).toDateString()}</td>
-                <td>{student.contactNumber}</td>
-                <td>{student.email}</td>
-                <td>
-                  <div className="action">
-                    <button id="delete">
-                      <Delete />
-                    </button>
-                    <button id="edit">
-                      <Edit />
-                    </button>
-                  </div>
-                </td>
+        <div className="">
+          <table cellPadding={0} cellSpacing={0} className="student-table">
+            <thead>
+              <tr>
+                <th>Username</th>
+                <th>Student ID</th>
+                <th>Enrollment Date</th>
+                <th>Date Birth</th>
+                <th>Contact Number</th>
+                <th>Email</th>
+                <th>Action</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {students.map(student => (
+                <tr key={student.id}>
+                  <td>
+                    {student.firstName} {student.lastName}
+                  </td>
+                  <td>{student.studentId}</td>
+                  <td>{new Date(student.enrollmentDate).toDateString()}</td>
+                  <td>{new Date(student.dateOfBirth).toDateString()}</td>
+                  <td>{student.contactNumber}</td>
+                  <td>{student.email}</td>
+                  <td>
+                    <div className="action flex">
+                      <button id="delete">
+                        <Delete />
+                      </button>
+                      <button id="edit">
+                        <Edit />
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
