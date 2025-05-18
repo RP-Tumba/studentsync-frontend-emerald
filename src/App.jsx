eb1957cf2fa8ad09e1065aa792cb1cc54fac8ca5
dosite-dev <dositecyiza@gmail.com>
2025-05-18 18:38:03 +0200

    fix: linting and formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import AddStudent from './pages/AddStudent';
+import AllStudents from './pages/AllStudents';
+import StudentDetails from './pages/StudentDetails';
+
 import './App.css';
 
 const App = () => {
@@ -8,6 +12,7 @@ const App = () => {
     <Router>
       <div className="app-container">
         <Navbar />
+
         <main className="main-content">
           <Routes>
             <Route path="/" element={<Home />} />
