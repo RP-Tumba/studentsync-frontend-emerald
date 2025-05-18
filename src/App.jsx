b8ad92abed595e0d0772620161f9bdbbeb731ffd
dosite-dev <dositecyiza@gmail.com>
2025-05-18 19:37:21 +0200

    feat: fix add form structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import Home from './pages/Home';
 import AddStudent from './pages/AddStudent';
-import AllStudents from './pages/Allstudents';
+import AllStudents from './pages/AllStudents';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import StudentList from './pages/StudentList';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import StudentDetails from './components/StudentDetails';
+import StudentDetails from './pages/StudentDetails';
 import './index.css';
 import './App.css';
-
-// import StudentDetails from './components/StudentDetails';
-// import CreateForm from './pages/CreateForm';
 
 const App = () => {
   return (
@@ -20,12 +16,9 @@ const App = () => {
         <main className="main-content">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/create-students" element={<AddStudent />} />
             <Route path="/students" element={<AllStudents />} />
             <Route path="/create-student" element={<AddStudent />} />
-            <Route path="/students" element={<AllStudents />} />
-            {<Route path="/students/:id" element={<StudentDetails />} />}
-            {<Route path="/students" element={<StudentList />} />}
+            <Route path="/students/:id" element={<StudentDetails />} />
           </Routes>
         </main>
 
--- src/pages/AddStudent.jsx
+++ src/pages/AddStudent.jsx
@@ -1,9 +1,80 @@
-import CreateForm from './CreateForm';
+import { useForm } from 'react-hook-form';
+import useStudentStore from '../store/studentStore';
 
 const AddStudent = () => {
+  const { error, addStudent } = useStudentStore();
+  const { register, handleSubmit } = useForm();
+  const onSubmit = async data => {
+    await addStudent(data);
+  };
+
   return (
     <div>
-      <CreateForm />
+      {error}
+      <form className="form-container" onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-header">
+          <div className="form-up-header">
+            <h1 className="text-gray">STUDENTSYNC</h1>
+          </div>
+          <br />
+          <h3>Add new student</h3>
+        </div>
+        <div className="form-inputs">
+          <div className="form-inputs-up">
+            <div>
+              <label>First Name</label>
+              <br />
+              <input type="text" {...register('firstName')} />
+              <br />
+            </div>
+
+            <div>
+              <label>Data Of Brith</label>
+              <br />
+              <input type="date" {...register('dateOfBirth')} />
+              <br />
+            </div>
+            <div>
+              <label>Last Name</label>
+              <br />
+              <input type="text" {...register('lastName')} />
+              <br />
+            </div>
+            <div>
+              <label>Student ID</label> <br />
+              <input type="text" {...register('studentId')} />
+              <br />
+            </div>
+          </div>
+          <div>
+            <label>Email</label>
+            <br />
+            <input type="email" {...register('email')} className="form-inputs-large" />
+            <br />
+          </div>
+          <div>
+            <label>Contact Number</label>
+            <br />
+            <input type="number" {...register('contactNumber')} className=" form-inputs-large" />
+            <br />
+          </div>
+          <div>
+            <label>enrollment date</label>
+            <br />
+            <input type="date" {...register('enrollmentDate')} className="form-inputs-large" />
+          </div>
+        </div>
+        <div className="form-btn">
+          <div>
+            <button type="submit" className="form-btn-style-green">
+              Add
+            </button>
+          </div>
+          <div>
+            <button className="form-btn-style-white">Cancel</button>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
